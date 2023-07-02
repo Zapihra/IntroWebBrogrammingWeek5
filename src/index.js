bc43e2a2a9b6418708fb62810fe3184fa30e16b0
Zapihra <iida.vaaja@student.lut.fi>
2023-07-02 19:51:45 +0300

    tooltip usage

--- src/index.js
+++ src/index.js
@@ -2,33 +2,37 @@ import L, { latLng, map } from "leaflet";
 import "./styles.css";
 
 
+const mapping = async () => {
+  fetch("https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326")
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
 
-fetch("https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326")
-  .then(response => response.json())
-  .then(data => {
-    //console.log(data);
+      let map = L.map('map').setView([61.05, 28.1], 14);
 
-    let map = L.map('map').setView([61.05, 28.1], 14);
+      let geojson = L.geoJSON(data, {
+        onEachFeature: getFeature,
+        style: {weight: 3}
+      }).addTo(map);
 
-    const getFeature = (feature, layer) => {
-      if (!feature.properties.nimi) return;
-      const name = feature.properties.nimi;
+      const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+      let osm = L.tileLayer(url, {
+        minZoom: -3,
+        attribution: "© OpenStreetMap"
+      }).addTo(map);
+
+      map.fitBounds(geojson.getBounds());
       
-      layer.bindTooltip(name);
-    }
+    });
+}
 
-    let geojson = L.geoJSON(data, {
-      onEachFeature: getFeature,
-      style: {weight: 3}
-    }).addTo(map);
+const getFeature = (feature, layer) => {
+  if (!feature.properties.nimi) return;
+  const name = feature.properties.nimi;
+  
+  layer.bindPopup(name);
+  layer.bindTooltip(name);
+};
 
-
-    let osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      minZoom: -3,
-      attribution: "© OpenStreetMap"
-    }).addTo(map);
-
-    map.fitBounds(geojson.getBounds());
-    
-  })
-
+mapping()
